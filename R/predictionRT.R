99b9914d11bbfde4b6bf46f6569eb5cb3f02183b
Chun Xuan <56767365+chunxuan98@users.noreply.github.com>
2022-04-11 14:36:39 +0800

    Update predictionRT.R

--- R/predictionRT.R
+++ R/predictionRT.R
@@ -40,7 +40,7 @@ getdesc_nopp <- function(input){
 predictionRT <- function(input){
   descs <- getdesc_nopp(input) #get mds without any data imputation
   #find a way to load colnames data, settings,and model
-  devtools::use_data(ntaRTmodel, ntaRTdescs, ntaRTnormsettings)
+  usethis::use_data(ntaRTmodel, ntaRTdescs, ntaRTnormsettings)
   #load.Rdata('ntaRTmodel.RData','predmodel')
   #load.Rdata('ntaRTdescs.RData','descname')
   #load.Rdata('ntaRTnormsettings.RData','settings')
